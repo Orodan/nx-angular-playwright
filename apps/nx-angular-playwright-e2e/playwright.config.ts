aa2b29568ef5d5ed94b48295d314d44276d8afeb
Jimmy Kasprzak <jimmykasprzak@gmail.com>
2025-03-17 15:18:30 +0100

    :wrench: Update Playwright config to get host ip dynamically and automatically

--- apps/nx-angular-playwright-e2e/playwright.config.ts
+++ apps/nx-angular-playwright-e2e/playwright.config.ts
@@ -2,8 +2,36 @@ import { defineConfig, devices } from '@playwright/test';
 import { nxE2EPreset } from '@nx/playwright/preset';
 import { workspaceRoot } from '@nx/devkit';
 
+import { execSync } from 'child_process';
+import { platform } from 'os';
+
+// Permet d'assurer que la baseUrl pour Playwright sera correct, que les tests soient exécuté depuis
+// des navigateurs locaux où dans un container.
+const getHostIP = () => {
+  try {
+    if (platform() === 'darwin') { 
+      // macOS : Trouver l'IP locale
+      return execSync("ifconfig | grep 'inet ' | grep -v 127.0.0.1 | awk '{print $2}'")
+        .toString().split("\n")[0].trim();
+    } else if (platform() === 'linux') { 
+      // Linux (Ubuntu, Debian...) : Trouver l'IP locale
+      return execSync("ip route | grep default | awk '{print $3}'")
+        .toString().trim();
+    } else if (platform() === 'win32') {
+      // Windows : Trouver l'IP locale
+      return execSync("powershell -Command \"(Get-NetIPAddress -AddressFamily IPv4 | Where-Object {$_.InterfaceAlias -NotLike 'Loopback*'}).IPAddress | Select-Object -First 1\"")
+        .toString().trim();
+    } else {
+      throw new Error(`OS non supporté: ${platform()}`);
+    }
+  } catch (e) {
+    console.error("Erreur lors de la récupération de l'IP de l'hôte", e);
+    process.exit(1);
+  }
+};
+
 // For CI, you may want to set BASE_URL to the deployed application.
-const baseURL = process.env['BASE_URL'] || 'http://localhost:4200';
+const baseURL = process.env['BASE_URL'] || `http://${getHostIP()}:4200`;
 
 /**
  * Read environment variables from file.
@@ -27,9 +55,9 @@ export default defineConfig({
   },
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: 'npx nx run nx-angular-playwright:serve',
+    command: 'npx nx run nx-angular-playwright:serve -- --host 0.0.0.0',
     url: 'http://localhost:4200',
-    reuseExistingServer: !process.env.CI,
+    reuseExistingServer: !process.env['CI'],
     cwd: workspaceRoot,
   },
   projects: [
